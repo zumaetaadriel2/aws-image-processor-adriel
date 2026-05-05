3442548d341b0ef85d55819878497e9d75b8a4d7
adriel <zumaetaadriel2@gmail.com>
2026-05-04 21:16:02 -0500

    fix: terraform format

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,4 +1,21 @@
-variable "aws_region" { default = "us-east-1" }
-variable "environment" {}
-variable "project_name" { default = "adriel-img-proc" }
-variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "aws_region" {
+  description = "Región de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Entorno (dev, qa, prod)"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Nombre del proyecto"
+  type        = string
+  default     = "adriel-img-proc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block para la VPC"
+  type        = string
+}
