11a1291fa25bd9e0fefc82df23ed939f85407e3a
adriel <zumaetaadriel2@gmail.com>
2026-05-04 21:25:04 -0500

    fix: terraform format

--- iac/network.tf
+++ iac/network.tf
@@ -2,17 +2,26 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = "${var.project_name}-vpc-${var.environment}" }
+  tags = {
+    Name = "${var.project_name}-vpc-${var.environment}"
+  }
 }
 
 # --- IGW y NATs ---
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = { Name = "${var.project_name}-igw" }
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
 }
 
-resource "aws_eip" "nat_a" { domain = "vpc" }
-resource "aws_eip" "nat_b" { domain = "vpc" }
+resource "aws_eip" "nat_a" {
+  domain = "vpc"
+}
+
+resource "aws_eip" "nat_b" {
+  domain = "vpc"
+}
 
 # --- SUBNETS PÚBLICAS Y NAT GATEWAYS ---
 resource "aws_subnet" "pub_a" {
@@ -50,55 +59,98 @@ resource "aws_subnet" "priv_b" {
 # --- ROUTING ---
 resource "aws_route_table" "rt_pub" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0", gateway_id = aws_internet_gateway.igw.id }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
-resource "aws_route_table_association" "pub_a" { subnet_id = aws_subnet.pub_a.id, route_table_id = aws_route_table.rt_pub.id }
-resource "aws_route_table_association" "pub_b" { subnet_id = aws_subnet.pub_b.id, route_table_id = aws_route_table.rt_pub.id }
+resource "aws_route_table_association" "pub_a" {
+  subnet_id      = aws_subnet.pub_a.id
+  route_table_id = aws_route_table.rt_pub.id
+}
+
+resource "aws_route_table_association" "pub_b" {
+  subnet_id      = aws_subnet.pub_b.id
+  route_table_id = aws_route_table.rt_pub.id
+}
 
 resource "aws_route_table" "rt_priv_a" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0", nat_gateway_id = aws_nat_gateway.nat_a.id }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
 }
-resource "aws_route_table_association" "priv_a" { subnet_id = aws_subnet.priv_a.id, route_table_id = aws_route_table.rt_priv_a.id }
+
+resource "aws_route_table_association" "priv_a" {
+  subnet_id      = aws_subnet.priv_a.id
+  route_table_id = aws_route_table.rt_priv_a.id
+}
 
 resource "aws_route_table" "rt_priv_b" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0", nat_gateway_id = aws_nat_gateway.nat_b.id }
-}
-resource "aws_route_table_association" "priv_b" { subnet_id = aws_subnet.priv_b.id, route_table_id = aws_route_table.rt_priv_b.id }
-
-# --- SECURITY GROUPS ---
-resource "aws_security_group" "sg_upload" {
-  name   = "sg-upload-lambda-${var.environment}"
-  vpc_id = aws_vpc.main.id
-  egress { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
 }
 
-resource "aws_security_group" "sg_crop" {
-  name   = "sg-crop-lambda-${var.environment}"
-  vpc_id = aws_vpc.main.id
-  egress { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+resource "aws_route_table_association" "priv_b" {
+  subnet_id      = aws_subnet.priv_b.id
+  route_table_id = aws_route_table.rt_priv_b.id
 }
 
-resource "aws_security_group" "sg_vpce_sqs" {
-  name   = "sg-vpce-sqs-${var.environment}"
-  vpc_id = aws_vpc.main.id
-  ingress { from_port = 443, to_port = 443, protocol = "tcp", security_groups = [aws_security_group.sg_upload.id, aws_security_group.sg_crop.id] }
-}
-
-# --- ENDPOINTS ---
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.rt_priv_a.id, aws_route_table.rt_priv_b.id]
+  route_table_ids   = [
+    aws_route_table.rt_priv_a.id,
+    aws_route_table.rt_priv_b.id
+  ]
 }
 
+# --- SECURITY GROUPS ---
+resource "aws_security_group" "sg_vpce_sqs" {
+  name   = "sg-vpce-sqs-${var.environment}"
+  vpc_id = aws_vpc.main.id
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+}
+
+resource "aws_security_group" "sg_lambdas" {
+  name   = "sg-lambdas-${var.environment}"
+  vpc_id = aws_vpc.main.id
+  
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_vpce_sqs.id]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [aws_vpc_endpoint.s3.prefix_list_id]
+  }
+}
+
+#ENDPOINTS
 resource "aws_vpc_endpoint" "sqs" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.${var.aws_region}.sqs"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.priv_a.id, aws_subnet.priv_b.id]
+  subnet_ids          = [
+    aws_subnet.priv_a.id,
+    aws_subnet.priv_b.id
+  ]
   security_group_ids  = [aws_security_group.sg_vpce_sqs.id]
   private_dns_enabled = true
 }
