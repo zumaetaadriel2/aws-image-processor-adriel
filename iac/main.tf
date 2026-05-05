935fdbbe2bdb851384ed2a6507803388e6a23049
adriel <zumaetaadriel2@gmail.com>
2026-05-04 21:17:21 -0500

    fix: terraform format

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
@@ -12,6 +15,7 @@ provider "aws" {
       Project     = "ImageProcessor"
       Environment = var.environment
       Author      = "AdrielZumaeta"
+      ManagedBy   = "Terraform"
     }
   }
 }
